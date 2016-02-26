0d102bbbece9547bd264e5e6ee13cdb2b1eeacac
iTyu <nagyityuistvan@gmail.com>
2016-02-26 14:12:18 +0100

    RED-4451

--- copy-gulpfile.js
+++ copy-gulpfile.js
@@ -5,7 +5,7 @@ var fs = require("fs-extra");
 process.chdir("./");
 
 try {
-	fs.copySync(__dirname + "/dotfiles", process.cwd() + "/");
+	fs.copy("/gulpfile.js", process.cwd() + "/gulpfile.js");
 } catch (err) {
 	console.error("Error: " + err.message);
 }
